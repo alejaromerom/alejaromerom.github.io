30aa72e0dd34f4cf1cf499101f959482c1c5d5dc
Alejandra Romero <aleromero43@gmail.com>
2022-05-11 14:13:23 -0500

    navbar and others properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import "./Assets/Img/mi-imagen.jpg";
+// import "./Assets/Img/mi-imagen.jpg";
 function App() {
 	return (
 		<div className="App">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<a class="navbar-brand" href="cv">
+				<a class="navbar-brand" href="">
 					About Me
 				</a>
 				<a class="navbar-brand" href="Aboutme">
@@ -26,9 +26,9 @@ function App() {
 				</button>
 			</nav>
 			<img
-				src="/src/Assets/Img/mi-imagen.jpg"
+				alt="mi-imagen"
+				src={require("./Assets/Img/mi-imagen.jpg")} // tengo que instalar más?
 				class="rounded float-left"
-				alt=""
 			></img>
 			<header className="App-header">
 				<p>Alejandra Romero</p>
