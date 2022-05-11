1a1353cca240147377fd3830748d4725afa6a651
Alejandra Romero <aleromero43@gmail.com>
2022-05-11 10:27:25 -0500

    creating navbar and properties

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./Assets/Img/mi-imagen.jpg";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				<a class="navbar-brand" href="cv">
+					About Me
+				</a>
+				<a class="navbar-brand" href="Aboutme">
+					Projects
+				</a>
+				<a class="navbar-brand" href="Aboutme">
+					Contact Me
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarText"
+					aria-controls="navbarText"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</nav>
+			<img
+				src="/src/Assets/Img/mi-imagen.jpg"
+				class="rounded float-left"
+				alt=""
+			></img>
+			<header className="App-header">
+				<p>Alejandra Romero</p>
+				<a
+					href="https://www.linkedin.com/in/alejaromerom/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					linkedin
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
